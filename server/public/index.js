4de471ca22fc74f4b6044ea01706006605483d7d
Lloydowski <janekbaszczynski@gmail.com>
2026-09-17 19:17:03 +0200

    hackclub time check

--- server/public/index.js
+++ server/public/index.js
@@ -1,15 +1,27 @@
 //@ts-nocheck
 
-/** @type {import("socket.io-client").Socket} */
-const socket = io("http://localhost:3000/player");
+// /** @type {import("socket.io-client").Socket} */
+// const socket = io("http://localhost:3000/player");
 
-socket.on("connect", () => {});
+// socket.on("connect", () => {
+// 	console.log("Web socket created");
 
-socket.on("roomInfo", (e) => {
-	console.log(e);
-});
+// 	const uuid = sessionStorage.getItem("userUUID");
+// 	socket.emit("registerUser", {
+// 		uuid: uuid,
+// 	});
+// });
 
-document.getElementById("join").addEventListener("click", () => {
-	const nick = document.getElementById("nick").value;
-	const roomID = document.getElementById("roomID").vale;
+document.getElementById("create").addEventListener("click", async () => {
+	const nick = document.getElementById("createNick").value;
+
+	sessionStorage.setItem("userNick", nick);
+
+	let response = await (
+		await fetch("/createRoom", {
+			method: "post",
+		})
+	).json();
+
+	window.location.replace("/game/" + response["roomID"]);
 });
